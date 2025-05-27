adce662f1d4c13ad65799672e66158c1af7f9f61
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-05-27 18:41:38 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -24,7 +24,7 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Kolkata").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://i.postimg.cc/4yVw6tm7/Picsart-23-03-26-11-08-19-025.jpg", "https://i.imgur.com/oHT3IGJ.jpeg", "https://i.imgur.com/iiLEaBT.jpeg", "https://i.imgur.com/ieuigWe.jpeg"];
+["https://i.imgur.com/AfRhbSc.jpeg", "https://i.imgur.com/oHT3IGJ.jpeg", "https://i.imgur.com/iiLEaBT.jpeg", "https://i.imgur.com/ieuigWe.jpeg"];
 var callback = () => api.sendMessage({body:`🌹𝙰𝙳𝙼𝙸𝙽 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🇮🇳 
 
 
