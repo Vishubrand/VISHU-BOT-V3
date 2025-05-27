7e7d4bf4b1454a746f05e27ca8b9780fcc7f80bd
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-05-27 18:13:11 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 😈 𝙍𝘼𝙅 𝙓𝙒𝘿 𝙏𝙃𝘼𝙆𝙐𝙍 `
+      body: `❤️${name},❤️  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 😈 𝙍𝘼𝙅 𝙓𝙒𝘿 𝙏𝙃𝘼𝙆𝙐𝙍 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
