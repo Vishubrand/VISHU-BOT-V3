2206343fd35b4cff8c8be78f7b3d8ed842185c8d
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-05-30 06:43:48 +0530

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -14,9 +14,9 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   if (event.body.indexOf("ADMIN")==0 || event.body.indexOf("Admin")==0 || event.body.indexOf("/Admin")==0 || event.body.indexOf("#admin")==0) {
     var msg = {
-        body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
+        body: "🫅 𝗥𝗔𝗝 𝗫𝗪𝗗 𝗧𝗛𝗔𝗞𝗨𝗥 🫅",
         attachment: 
-fs.createReadStream(__dirname + `/noprefix/profile.png`)
+fs.createReadStream(__dirname + `https://i.imgur.com/ieuigWe.jpeg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
